c75b1dd48b984e0166c887e340d59700f819f5a3
Marco Parisi <parmarco2004@gmail.com>
2026-06-10 15:44:58 +0200

    Improved UX
    Co-Authored-By: Simone Castelli <154014757+0dxplt@users.noreply.github.com>

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,18 +1,35 @@
-import { Component, EnvironmentInjector, inject } from '@angular/core';
+import { Component, EnvironmentInjector, inject, signal } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { triangle, ellipse, square, person, search, barbell, trophy, fitness} from 'ionicons/icons';
+import { NgClass } from '@angular/common';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
-  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
+  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, NgClass],
 })
 export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
-  constructor() {
+  profileClass = signal<"me" | "other">("me");
+
+  constructor(private router: Router) {
     addIcons({ triangle, ellipse, square, trophy, person, search, barbell, fitness});
+    this.router.events
+    .pipe(
+      filter(event => event instanceof NavigationEnd)
+    )
+    .subscribe(() => {
+      const regex = /\/tabs\/profile\/.+$/;
+      if (this.router.url.match(regex)) {
+        this.profileClass.set('other');
+      } else {
+        this.profileClass.set('me');
+      }
+    });
   }
 }
